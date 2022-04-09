2d2a84ac184c933021ca96c9e932f9aec1119a7b
Kaiser <kaiserlancelot123@gmail.com>
2022-04-09 10:49:33 +0800

    Add coverage

--- chapter02/src/main.rs
+++ chapter02/src/main.rs
@@ -1,3 +1,17 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", add_two(2));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::add_two;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add_two(2), 4);
+    }
 }
