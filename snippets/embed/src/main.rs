aa4c2a080eb9c1e142b705bd735182aaa295a78e
Kaiser <kaiserlancelot123@gmail.com>
2022-04-12 14:16:10 +0800

    Remove embed

--- snippets/embed/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let str = include_str!("../data/text.txt");
-    assert_eq!(str, "Hello World!\n");
-    println!("{}", str);
-}
--- snippets/font/src/main.rs
+++ snippets/font/src/main.rs
@@ -2,8 +2,8 @@ use allsorts::binary::read::ReadScope;
 use allsorts::font_data::FontData;
 
 pub fn main() {
-    let buffer = std::fs::read("SourceHanSansSC-Bold.otf").expect("Can not read file");
-    let _font_file = ReadScope::new(&buffer)
+    let data = include_bytes!("../data/SourceHanSansSC-Bold.otf");
+    let _font_file = ReadScope::new(data)
         .read::<FontData>()
         .expect("Can not read font");
 }
